65da6dc196ffdd99519eb21b9091bc59a4d03936
LAPTOP-9PSS9LRH\johns <johnsonjaurigue2@gmail.com>
2023-03-22 15:39:35 +0800

    chore: text editor automated formatting and some images for references
    no changes much other than files and formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
 
-			colors:{
-				'transparent':'transparent',
-				'esl-c':{
-					'1' : '#015CB9',
-					'2':'#23085A',
-					'3' :'#00DCFF',
-					'4': '#23085A',
-					'5' : '#CAECFF'
-				},
-			},
-			fontSize:{
-					'esl-1' : '0.75em',
-					'esl-2' : '1em',
-					'esl-3' : '1.33em',
-					'esl-4' : '1.77em',
-					'esl-5' : '2.369em',
-					'esl-6' : '3.157em',
-			}
+      colors: {
+        'transparent': 'transparent',
+        'esl-c': {
+          '1': '#015CB9',
+          '2': '#23085A',
+          '3': '#00DCFF',
+          '4': '#23085A',
+          '5': '#CAECFF'
+        },
+      },
+      fontSize: {
+        'esl-1': '0.75em',
+        'esl-2': '1em',
+        'esl-3': '1.33em',
+        'esl-4': '1.77em',
+        'esl-5': '2.369em',
+        'esl-6': '3.157em',
+      }
 
-		},
-		
-	},
-	plugins: [],
+    },
+
+  },
+  plugins: [],
 }
