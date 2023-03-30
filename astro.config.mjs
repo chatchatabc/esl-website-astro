e036f17235b4ece908febd6bd2457a4b42f0106a
icecloud12 <johnsonjaurigue2@gmail.com>
2023-03-30 18:06:49 +0800

    build: applying astro image tools with a test run

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 
 import svelte from "@astrojs/svelte";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), svelte()]
+  integrations: [tailwind(), astroI18next(), svelte(), astroImageTools]
 });
