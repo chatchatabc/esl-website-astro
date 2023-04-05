66b7e383d3d98abfea68c78f85d9d8b241c9e075
icecloud12 <johnsonjaurigue2@gmail.com>
2023-04-05 12:57:34 +0800

    feat: modified and compartmentalized the video selection

---
+++ src/components/YoutubeIframe.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let teacher: Record<string, any>;
+  let showIframe = false;
+</script>
+
+<div class="relative top-0 h-0 w-full pb-[50%]">
+  {#if showIframe}
+    <iframe
+      class="w-full h-full"
+      src={teacher.video}
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  {/if}
+
+  <div class="absolute h-full w-full youtube-thumbnail-button">
+    {#if !showIframe}
+      <button
+        on:click={() => (showIframe = true)}
+        class="absolute w-full h-full left-0 top-0"><slot /></button
+      >
+    {/if}
+  </div>
+</div>
