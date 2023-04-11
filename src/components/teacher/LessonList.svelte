1d44d1e55147d10f34ae46181a605edb9f391488
icecloud12 <johnsonjaurigue2@gmail.com>
2023-04-11 18:03:16 +0800

    refactor: creating a smaller component for refactoring

---
+++ src/components/teacher/LessonList.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Modal from "../common/Modal.svelte";
+  export let showModal = false;
+  export let lessons: Array<string>;
+
+  function toggleModal() {
+    showModal = !showModal;
+  }
+</script>
+
+<div class="bg-white p-5 mt-4">
+  <div class="space-y-4 sm:w-10/12 sm:mx-auto">
+    {#each lessons as lesson}
+      <!--lesson item -->
+      <div class="bg-white drop-shadow-xl p-4 flex justify-between">
+        <div class="text-esl-2">{lesson}</div>
+        <button
+          class="bg-[#FF4438] py-1 px-[2rem] rounded-full text-white"
+          on:click={toggleModal}
+        >
+          预约
+        </button>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<Modal bind:open={showModal}><slot name="img" /></Modal>
