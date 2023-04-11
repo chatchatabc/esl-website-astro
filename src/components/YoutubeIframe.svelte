8545d730dbdcc2ba1a1d4f032a3f576d5159b6d6
icecloud12 <johnsonjaurigue2@gmail.com>
2023-04-11 08:30:50 +0800

    style: automatic formatting

--- src/components/YoutubeIframe.svelte
+++ src/components/YoutubeIframe.svelte
@@ -1,44 +1,40 @@
 <script lang="ts">
   export let teacher: Record<string, any>;
-  
+
   let showIframe = false;
   let count = 0;
-  function BtnClick(event:any){
+  function BtnClick(event: any) {
     showIframe = true;
-   
   }
 </script>
 
-<div class="flex h-full w-full items-center">
-  <div class="w-full h-0 pb-[50%] relative">
-    {#if showIframe}
-      <iframe
-        class="w-full h-full absolute"
-        src={teacher.video}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      />
-    {/if}
-    {#if !showIframe}
-      <div class="absolute h-full w-full youtube-thumbnail-button">
-      
+<div class=" p-2 flex h-full w-full">
+  <div class="flex h-full w-full items-center">
+    <div class="w-full h-0 pb-[50%] relative">
+      {#if showIframe}
+        <iframe
+          class="w-full h-full absolute"
+          src={teacher.video}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        />
+      {/if}
+      {#if !showIframe}
+        <div class="absolute h-full w-full youtube-thumbnail-button">
           <button
             on:click={BtnClick}
-            class="absolute w-full h-full left-0 top-0">
+            class="absolute w-full h-full left-0 top-0"
+          >
             <img
               src={teacher.videoThumbnail}
               alt="Video"
               class="absolute w-[100%] h-[100%] top-0"
             />
-          </button
-          >
-        
-      </div>
-    {/if}
-
+          </button>
+        </div>
+      {/if}
+    </div>
   </div>
-  
 </div>
-
