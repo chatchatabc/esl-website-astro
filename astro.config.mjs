2f94ce7ac06675705b8e73ad895e935850b0ed5f
BoJoNVi <bonjomontes@gmail.com>
2023-07-13 14:01:14 +0800

    build: added sitemap and robots txt generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
-
 import svelte from "@astrojs/svelte";
 import { astroImageTools } from "astro-imagetools";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), svelte(), astroImageTools],
+  site: "https://esl-cca.pages.dev",
+  integrations: [
+    tailwind(),
+    astroI18next(),
+    svelte(),
+    astroImageTools,
+    sitemap(),
+    robotsTxt(),
+  ],
 });
