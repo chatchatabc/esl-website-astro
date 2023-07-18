861a50b53e886be230718da572460e29ab62613a
BoJoNVi <bonjomontes@gmail.com>
2023-07-18 16:23:01 +0800

    feat(client): implemented sign in operation

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -1,1 +1,11 @@
-export async function authLogin() {}
+import trpc, { trpcAction } from "src/infra/trpc";
+import type {
+  User,
+  UserLogin,
+} from "../../../server/src/domain/models/UserModel";
+
+export async function authLogin(input: UserLogin) {
+  const response = await trpcAction<User>(trpc.auth.login.query, input);
+
+  return response;
+}
