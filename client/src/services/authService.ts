dd25b02a7ce1613a58748186b3f7a960c242c105
BoJoNVi <bonjomontes@gmail.com>
2023-07-18 17:19:42 +0800

    feat(client): added service to register

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -2,10 +2,17 @@ import trpc, { trpcAction } from "src/infra/trpc";
 import type {
   User,
   UserLogin,
+  UserRegister,
 } from "../../../server/src/domain/models/UserModel";
 
-export async function authLogin(input: UserLogin) {
-  const response = await trpcAction<User>(trpc.auth.login.query, input);
+export async function authLogin(data: UserLogin) {
+  const response = await trpcAction<User>(trpc.auth.login.query, data);
+
+  return response;
+}
+
+export async function authRegister(data: UserRegister) {
+  const response = await trpcAction<User>(trpc.auth.register.mutate, data);
 
   return response;
 }
