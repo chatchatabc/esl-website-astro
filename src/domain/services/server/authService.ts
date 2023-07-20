f79237ec4f265e5fc8cabca05b984d660bdd1717
BoJoNVi <bonjomontes@gmail.com>
2023-07-20 11:05:12 +0800

    improv: added auth validation using jwt signature

--- src/domain/services/server/authService.ts
+++ src/domain/services/server/authService.ts
@@ -1,46 +1,61 @@
 import { userDbGetByUsername, userDbInsert } from "../../repositories/userRepo";
-import CryptoJS, { SHA256 } from "crypto-js";
+import CryptoJS from "crypto-js";
 import { utilFailedResponse } from "../server/utilService";
 import type { User, UserRegister } from "../../models/UserModel";
 import type { TrpcResponse } from "../../models/TrpcModel";
 import type { TrpcContext } from "../../infra/trpcServerActions";
 
-const secret = "secret";
+const secret = "I)0Don't!1Care@2";
+const jwtHeader = JSON.stringify({ alg: "HS256", typ: "JWT" });
+const base64Header = Buffer.from(jwtHeader).toString("base64");
 
-export function authCreateHash(password: string) {
-  return SHA256(password + secret).toString();
+export function authCreateHash(value: string) {
+  return CryptoJS.HmacSHA256(value, secret).toString();
 }
 
-export function authCreateToken(payload: Record<string, any>) {
+export function authCreateToken(id: number) {
   const iat = Math.floor(Date.now() / 1000);
   const exp = iat + 60 * 60 * 24 * 7; // 7 days
 
-  const data = {
-    iat,
-    exp,
-    payload,
-  };
+  const jwtPayload = JSON.stringify({ exp, id });
+  const base64Payload = Buffer.from(jwtPayload).toString("base64");
+  const signature = authCreateHash(`${base64Header}.${base64Payload}`);
 
-  return CryptoJS.AES.encrypt(JSON.stringify(data), secret).toString();
+  return `${base64Header}.${base64Payload}.${signature}`;
 }
 
-export function authDecodeToken(token: string) {
+export function authValidateToken(token: string) {
+  const [header, payload, signature] = token.split(".");
+  if (!header || !payload || !signature) {
+    return false;
+  }
+
+  const authSignature = authCreateHash(`${header}.${payload}`);
+  if (signature !== authSignature) {
+    return false;
+  }
+
+  return true;
+}
+
+export function authGetTokenPayload(token: string) {
   if (token.startsWith("Bearer ")) {
-    token = token.slice(7, token.length);
+    token.slice("bearer ".length);
   }
 
-  const bytes = CryptoJS.AES.decrypt(token, secret);
-  const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8)) as {
-    iat: number;
-    exp: number;
-    payload: Record<string, any>;
-  };
-
-  if (data.exp < Math.floor(Date.now() / 1000)) {
-    throw utilFailedResponse("Token expired", 401);
+  if (!authValidateToken(token)) {
+    return null;
   }
 
-  return data.payload;
+  const payload = token.split(".")[1];
+  const data = Buffer.from(payload, "base64").toString();
+  const obj = JSON.parse(data) as { id: number; exp: number };
+
+  if (obj.exp < Date.now()) {
+    return null;
+  }
+
+  return obj.id;
 }
 
 export async function authRegister(input: UserRegister, ctx: TrpcContext) {
