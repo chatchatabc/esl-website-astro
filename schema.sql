e61edae8acc9ae959431295db858b69d73147070
BoJoNVi <bonjomontes@gmail.com>
2023-07-20 10:17:14 +0800

    feat: added schedules table in the schema

--- schema.sql
+++ schema.sql
@@ -4,6 +4,10 @@ CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL
 INSERT INTO users (username, password, createdAt, updatedAt, role) VALUES ('admin', 'da023f7090dd831097f8a534475b1c4fba2a9a6419968e52be7459e2533ac819', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1);
 
 DROP TABLE IF EXISTS roles;
-CREATE TABLE IF NOT EXISTS roles (id INTEGER PRIMARY KEY, name TEXT, createdAt TEXT, updatedAt TEXT);
-INSERT INTO roles (id, name, createdAt, updatedAt) VALUES (1, 'student', '2019-01-01 00:00:00', '2019-01-01 00:00:00'), (2, 'teacher', '2019-01-01 00:00:00', '2019-01-01 00:00:00');
+CREATE TABLE IF NOT EXISTS roles (id INTEGER PRIMARY KEY, name TEXT, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL);
+INSERT INTO roles (name, createdAt, updatedAt) VALUES ('student', '2019-01-01 00:00:00', '2019-01-01 00:00:00'), ('teacher', '2019-01-01 00:00:00', '2019-01-01 00:00:00');
 
+-- Columns: id, teacherId, userId, startDate, endDate, status, createdAt, updatedAt
+DROP TABLE IF EXISTS schedules;
+CREATE TABLE IF NOT EXISTS schedules (id INTEGER PRIMARY KEY, teacherId INTEGER NOT NULL, startDate TEXT NOT NULL, endDate TEXT NOT NULL, userId INTEGER, status INTEGER NOT NULL, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL);
+INSERT INTO schedules (teacherId, userId, status, createdAt, updatedAt, startDate, endDate) VALUES (2, 1, 0, '2019-01-01 00:00:00', '2019-01-01 00:00:00', '2019-01-01 00:00:00', '2019-01-01 00:00:00');
