692886e8c958a2df248790eee46ba4cf72769341
BoJoNVi <bonjomontes@gmail.com>
2023-07-20 11:37:35 +0800

    refactor: changed the inserted user password due to change of secret

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 -- Columns: id, username, password, phone, email, firstName, lastName, role, createdAt, updatedAt
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, phone TEXT, email TEXT, firstName TEXT, lastName TEXT, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL, role INTEGER NOT NULL);
-INSERT INTO users (username, password, createdAt, updatedAt, role) VALUES ('admin', 'da023f7090dd831097f8a534475b1c4fba2a9a6419968e52be7459e2533ac819', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1);
+INSERT INTO users (username, password, createdAt, updatedAt, role) VALUES ('admin', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', '2019-01-01 00:00:00', '2019-01-01 00:00:00', 1);
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE IF NOT EXISTS roles (id INTEGER PRIMARY KEY, name TEXT, createdAt TEXT NOT NULL, updatedAt TEXT NOT NULL);
