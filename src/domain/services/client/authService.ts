997c8a8bcf8f882f91cd4d464e4513ca4adff627
BoJoNVi <bonjomontes@gmail.com>
2023-07-24 13:53:44 +0800

    fix: can't get the userId in session storage

--- src/domain/services/client/authService.ts
+++ src/domain/services/client/authService.ts
@@ -3,8 +3,6 @@ import type {
   UserLogin,
   UserRegister,
 } from "src/domain/models/UserModel";
-import type { AxiosResponse } from "src/domain/models/AxiosModel";
-import { restPost } from "src/domain/infra/restActions";
 import { trpcClient } from "src/domain/infra/trpcClientActions";
 
 export async function authLogin(data: UserLogin) {
@@ -24,9 +22,10 @@ export function authLogout() {
 }
 
 export async function authRegister(data: UserRegister) {
-  const response: AxiosResponse<User> = await restPost("/auth/register", data);
+  // const response: AxiosResponse<User> = await restPost("/auth/register", data);
 
-  return response.data;
+  // return response.data;
+  return null;
 }
 
 export function authGetUserId() {
@@ -36,5 +35,5 @@ export function authGetUserId() {
     return null;
   }
 
-  return userId.split("=")[1];
+  return userId;
 }
