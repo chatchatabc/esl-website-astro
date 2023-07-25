56b2bfae4aaa669073fd228d8bc32cb68e4587be
BoJoNVi <bonjomontes@gmail.com>
2023-07-25 14:43:39 +0800

    feat(client): implemented service for updating teacher schedule

--- src/domain/services/client/authService.ts
+++ src/domain/services/client/authService.ts
@@ -35,5 +35,5 @@ export function authGetUserId() {
     return null;
   }
 
-  return userId;
+  return Number(userId);
 }
--- src/domain/services/client/scheduleService.ts
+++ src/domain/services/client/scheduleService.ts
@@ -1,8 +1,24 @@
 import { trpcClient } from "src/domain/infra/trpcClientActions";
+import type { Schedule } from "src/domain/models/ScheduleModel";
 
 export async function scheduleGetAllByUser(params: { userId: number }) {
   try {
     const response = await trpcClient.schedule.getAllByUser.query(params);
+    return response;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
+export async function scheduleUpdateMany(params: {
+  userId: number;
+  schedules: Schedule[];
+}) {
+  try {
+    const response = await trpcClient.schedule.updateManyByTeacher.mutate(
+      params
+    );
     return response;
   } catch (e) {
     console.log(e);
