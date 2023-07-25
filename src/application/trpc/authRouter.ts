9ea3d692b141220054f3584425c6025428a07587
BoJoNVi <bonjomontes@gmail.com>
2023-07-25 10:10:49 +0800

    refactor: removed middleware for the authRouter

--- src/application/trpc/authRouter.ts
+++ src/application/trpc/authRouter.ts
@@ -1,7 +1,4 @@
-import {
-  trpcProcedure,
-  trpcRouterCreate,
-} from "src/domain/infra/trpcServerActions";
+import { trpc, trpcRouterCreate } from "src/domain/infra/trpcServerActions";
 import type { UserLogin } from "src/domain/models/UserModel";
 import {
   authCreateToken,
@@ -9,25 +6,21 @@ import {
 } from "src/domain/services/server/authService";
 
 export default trpcRouterCreate({
-  login: trpcProcedure
+  login: trpc.procedure
     .input((values) => {
       const data = values as UserLogin;
-
       if (!data.username || !data.password) {
         throw new Error("Missing username or password");
       }
-
       return data;
     })
     .mutation(async (opts) => {
       const user = await authLogin(opts.input, opts.ctx.env);
       const token = authCreateToken(user.id);
-
       opts.ctx.resHeaders.append(
         "Set-Cookie",
         `token=${token}; Path=/; SameSite=None; Secure; HttpOnly`
       );
-
       return user;
     }),
 });
