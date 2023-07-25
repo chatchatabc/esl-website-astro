7bad4015bf358848035fb599d184e73431691414
BoJoNVi <bonjomontes@gmail.com>
2023-07-25 10:10:08 +0800

    feat: implemented editing of schedule blocks

--- src/components/profiles/TeacherSchedule.svelte
+++ src/components/profiles/TeacherSchedule.svelte
@@ -1,13 +1,51 @@
 <script lang="ts">
   import { Calendar } from "@fullcalendar/core";
+  import interactionPlugin from "@fullcalendar/interaction";
   import timeGridPlugin from "@fullcalendar/timegrid";
   import { bookingGetAllByUser } from "src/domain/services/client/bookingService";
   import { scheduleGetAllByUser } from "src/domain/services/client/scheduleService";
   import { onMount } from "svelte";
 
+  let calendar: Calendar | null = null;
+
   let bookings = [] as any[];
   let schedules = [] as any[];
-  let create = false;
+  let editing = false;
+
+  $: if (editing && calendar) {
+    calendar?.setOption("editable", true);
+    schedules.forEach((event, index) => {
+      const inactiveEvent = {
+        ...event,
+        id: `inactive-${index}`,
+        color: "red",
+        display: "inverse-background",
+      };
+      const activeEvent = {
+        ...event,
+        id: `active-${index}`,
+        display: "block",
+      };
+      delete activeEvent.title;
+      delete inactiveEvent.title;
+      calendar?.addEvent(inactiveEvent);
+      calendar?.addEvent(activeEvent);
+      calendar?.getEventById(`open-${index}`)?.remove();
+    });
+    bookings.forEach((_, index) => {
+      calendar?.getEventById(`bk-${index}`)?.remove();
+    });
+  } else if (calendar) {
+    calendar?.setOption("editable", false);
+    schedules.forEach((event, index) => {
+      calendar?.getEventById(`inactive-${index}`)?.remove();
+      calendar?.getEventById(`active-${index}`)?.remove();
+      calendar?.addEvent(event);
+    });
+    bookings.forEach((event) => {
+      calendar?.addEvent(event);
+    });
+  }
 
   onMount(async () => {
     const responseSchedules = await scheduleGetAllByUser({ userId: 3 });
@@ -15,7 +53,7 @@
     if (responseSchedules) {
       schedules = Object.values(responseSchedules)
         .flat()
-        .map((event) => {
+        .map((event, index) => {
           const start = new Date(event.startTime);
           const end = new Date(event.endTime);
 
@@ -29,7 +67,8 @@
             .padStart(2, "0")}`;
 
           return {
-            title: "Free Schedule",
+            id: `open-${index}`,
+            title: "Open Schedule",
             startTime,
             endTime,
             display: "background",
@@ -40,11 +79,11 @@
     if (responseBookings) {
       bookings = Object.values(responseBookings.content)
         .flat()
-        .map((event) => {
+        .map((event, index) => {
           const start = new Date(event.start);
           const end = new Date(event.end);
-
           return {
+            id: `bk-${index}`,
             title: "Booked Schedule",
             start,
             end,
@@ -57,15 +96,15 @@
       "[data-teacher-calendar]"
     )!;
 
-    let calendar = new Calendar(calendarEl, {
-      plugins: [timeGridPlugin],
+    calendar = new Calendar(calendarEl, {
+      plugins: [timeGridPlugin, interactionPlugin],
+
       initialView: "timeGridWeek",
-      events: [...schedules, ...bookings],
       customButtons: {
         add: {
-          text: "Create +",
+          text: "Edit",
           click: () => {
-            create = !create;
+            editing = !editing;
           },
         },
       },
