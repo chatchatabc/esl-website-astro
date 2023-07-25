6a429060f0a06464afae349d1d6a343ac058d197
BoJoNVi <bonjomontes@gmail.com>
2023-07-25 10:55:07 +0800

    refactor: changed the way how schedules is received in the front-end

--- src/components/profiles/TeacherSchedule.svelte
+++ src/components/profiles/TeacherSchedule.svelte
@@ -2,33 +2,31 @@
   import { Calendar } from "@fullcalendar/core";
   import interactionPlugin from "@fullcalendar/interaction";
   import timeGridPlugin from "@fullcalendar/timegrid";
+  import type { Booking } from "src/domain/models/BookingModel";
+  import type { Schedule } from "src/domain/models/ScheduleModel";
   import { bookingGetAllByUser } from "src/domain/services/client/bookingService";
   import { scheduleGetAllByUser } from "src/domain/services/client/scheduleService";
   import { onMount } from "svelte";
 
   let calendar: Calendar | null = null;
 
-  let bookings = [] as any[];
-  let schedules = [] as any[];
+  let bookingsEvent = [] as Record<string, any>[];
+  let bookings = [] as Booking[];
+  let schedulesEvent = [] as Record<string, any>[];
+  let schedules = [] as Schedule[];
   let editing = false;
 
   $: if (editing && calendar) {
     calendar?.setOption("editable", true);
-    schedules.forEach((event, index) => {
-      const inactiveEvent = {
-        ...event,
-        id: `inactive-${index}`,
-        color: "red",
-        display: "inverse-background",
-      };
+    calendar?.setOption("selectable", true);
+    schedulesEvent.forEach((event, index) => {
       const activeEvent = {
         ...event,
+        title: undefined,
         id: `active-${index}`,
         display: "block",
       };
       delete activeEvent.title;
-      delete inactiveEvent.title;
-      calendar?.addEvent(inactiveEvent);
       calendar?.addEvent(activeEvent);
       calendar?.getEventById(`open-${index}`)?.remove();
     });
@@ -37,60 +35,52 @@
     });
   } else if (calendar) {
     calendar?.setOption("editable", false);
-    schedules.forEach((event, index) => {
-      calendar?.getEventById(`inactive-${index}`)?.remove();
+    calendar?.setOption("selectable", false);
+    schedulesEvent.forEach((event, index) => {
       calendar?.getEventById(`active-${index}`)?.remove();
       calendar?.addEvent(event);
     });
-    bookings.forEach((event) => {
+    bookingsEvent.forEach((event) => {
       calendar?.addEvent(event);
     });
   }
 
   onMount(async () => {
-    const responseSchedules = await scheduleGetAllByUser({ userId: 3 });
-    const responseBookings = await bookingGetAllByUser({ userId: 3 });
-    if (responseSchedules) {
-      schedules = Object.values(responseSchedules)
-        .flat()
-        .map((event, index) => {
-          const start = new Date(event.startTime);
-          const end = new Date(event.endTime);
+    schedules = (await scheduleGetAllByUser({ userId: 3 }))?.content ?? [];
+    bookings = (await bookingGetAllByUser({ userId: 3 }))?.content ?? [];
+    schedulesEvent = schedules.map((event, index) => {
+      const start = new Date(event.startTime);
+      const end = new Date(event.endTime);
+      // 24h
+      const timeFormatter = new Intl.DateTimeFormat("en", {
+        timeStyle: "short",
+        hourCycle: "h23",
+      });
 
-          const startTime = `${start.getHours()}:${start
-            .getMinutes()
-            .toString()
-            .padStart(2, "0")}`;
-          const endTime = `${end.getHours()}:${end
-            .getMinutes()
-            .toString()
-            .padStart(2, "0")}`;
+      const startTime = timeFormatter.format(start);
+      const endTime = timeFormatter.format(end);
+      console.log(startTime, endTime);
 
-          return {
-            id: `open-${index}`,
-            title: "Open Schedule",
-            startTime,
-            endTime,
-            display: "background",
-            daysOfWeek: [event.day],
-          };
-        });
-    }
-    if (responseBookings) {
-      bookings = Object.values(responseBookings.content)
-        .flat()
-        .map((event, index) => {
-          const start = new Date(event.start);
-          const end = new Date(event.end);
-          return {
-            id: `bk-${index}`,
-            title: "Booked Schedule",
-            start,
-            end,
-            color: "red",
-          };
-        });
-    }
+      return {
+        id: `open-${index}`,
+        title: "Open Schedule",
+        startTime,
+        endTime,
+        display: "background",
+        daysOfWeek: [event.day],
+      };
+    });
+    bookingsEvent = bookings.map((event, index) => {
+      const start = new Date(event.start);
+      const end = new Date(event.end);
+      return {
+        id: `bk-${index}`,
+        title: "Booked Schedule",
+        start,
+        end,
+        color: "red",
+      };
+    });
 
     const calendarEl = document.querySelector<HTMLElement>(
       "[data-teacher-calendar]"
