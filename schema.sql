1e7d2dd2e157f0714346dc955f4fee58a2e0c0b1
BoJoNVi <bonjomontes@gmail.com>
2023-07-27 14:49:44 +0800

    feat: added a teachers table

--- schema.sql
+++ schema.sql
@@ -2,20 +2,31 @@
 * id (integer)
 * username (text)
 * password (text)
-* phone (text)
-* email (text)
-* firstName (text)
-* lastName (text)
+* phone (text?)
+* email (text?)
+* firstName (text?)
+* lastName (text?)
 * roleId (integer)
 * credit (integer)
+* createdAt (timestamp)
+* updatedAt (timestamp)
+*/
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, phone TEXT, email TEXT, firstName TEXT, lastName TEXT, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL, roleId INTEGER NOT NULL, credit INTEGER NOT NULL);
+INSERT INTO users (username, password, roleId, credit, createdAt, updatedAt) VALUES ('admin', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 1, 1000, 0, 0), ('student', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 2, 1000, 0, 0), ('teacher', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 3, 1000, 0, 0);
+
+/**
+* id (integer)
+* userId (integer)
 * price (integer)
 * interval (timestampTimeOnly)
 * createdAt (timestamp)
 * updatedAt (timestamp)
 */
-DROP TABLE IF EXISTS users;
-CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, phone TEXT, email TEXT, firstName TEXT, lastName TEXT, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL, roleId INTEGER NOT NULL, credit INTEGER NOT NULL, price INTEGER NOT NULL, interval INTEGER NOT NULL);
-INSERT INTO users (username, password, createdAt, updatedAt, roleId, credit) VALUES ('admin', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 0, 0, 1, 1000), ('student', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 0, 0, 2, 1000), ('teacher', 'd7cd68b6014e62d355e294a622fe95894f047ba5dfd8cc06f98122cc2bb945d3', 0, 0, 3, 1000);
+DROP TABLE IF EXISTS teachers;
+CREATE TABLE IF NOT EXISTS teachers (id INTEGER PRIMARY KEY, userId INTEGER UNIQUE, price INTEGER NOT NULL, interval INTEGER NOT NULL, createdAt INTEGER NOT NULL, updatedAt INTEGER NOT NULL);
+INSERT INTO teachers (userId, price, interval, createdAt, updatedAt) VALUES (3, 50, 25, 0, 0);
+
 
 /**
 * id (integer)
