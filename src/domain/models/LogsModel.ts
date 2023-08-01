99c296fa6141f4d347b49e5420b550ea0bfaf9d8
BoJoNVi <bonjomontes@gmail.com>
2023-08-01 09:34:38 +0800

    feat: LogsCredit model

---
+++ src/domain/models/LogsModel.ts
@@ -0,0 +1,9 @@
+export type LogsCredit = {
+  id?: number;
+  senderId?: number;
+  receiverId?: number;
+  amount?: number;
+  status?: number; // 0: pending, 1: accepted, 2: cancelled
+  createdAt?: number;
+  updatedAt?: number;
+};
