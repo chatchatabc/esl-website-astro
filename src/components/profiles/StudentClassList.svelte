4b0823988d5783fdc31f832e6935eadc75d84c31
BoJoNVi <bonjomontes@gmail.com>
2023-08-01 14:53:03 +0800

    feat(client): UI for cancelling a booking

--- src/components/profiles/StudentClassList.svelte
+++ src/components/profiles/StudentClassList.svelte
@@ -2,9 +2,15 @@
   import { Calendar } from "@fullcalendar/core";
   import listPlugin from "@fullcalendar/list";
   import type { Booking } from "src/domain/models/BookingModel";
-  import { bookingGetAll } from "src/domain/services/client/bookingService";
+  import {
+    bookingCancel,
+    bookingGetAll,
+  } from "src/domain/services/client/bookingService";
   import { onMount } from "svelte";
 
+  let loading = true;
+  let bookingId = 0;
+  let showModal = false;
   let calendarEl: any;
   let bookings = [] as Booking[];
   let calendar: Calendar | null;
@@ -18,6 +24,10 @@
         left: "",
         right: "",
       },
+      eventClick: (e) => {
+        bookingId = Number(e.event.id);
+        showModal = true;
+      },
     });
     calendar.render();
   }
@@ -27,6 +37,7 @@
       calendar.removeAllEvents();
       bookings.forEach((booking) => {
         calendar?.addEvent({
+          id: String(booking.id),
           title: booking.teacher?.username,
           start: booking.start,
           end: booking.end,
@@ -35,12 +46,70 @@
     }
   }
 
+  async function fetchData() {
+    const responseBooking = await bookingGetAll({});
+    if (responseBooking) {
+      bookings = responseBooking?.content ?? [];
+      generateClass();
+    }
+    loading = false;
+  }
+
+  async function handleCancel() {
+    const response = await bookingCancel({ bookingId });
+    if (!response) {
+      alert("Unable to cancel");
+    }
+    fetchData();
+    showModal = false;
+  }
+
   onMount(async () => {
-    const response = await bookingGetAll({});
-    bookings = response?.content ?? [];
-    generateClass();
+    fetchData();
   });
 </script>
+
+<!-- Modal -->
+<div
+  aria-hidden={showModal ? "false" : "true"}
+  aria-label="Modal Background"
+  on:click={(e) => {
+    if (e.target === e.currentTarget) {
+      showModal = false;
+    }
+  }}
+  class={`fixed top-0 left-0 bg-black bg-opacity-30 h-full w-full ${
+    showModal
+      ? "opacity-100 pointer-events-auto"
+      : "opacity-0 pointer-events-none"
+  } flex z-[5] justify-center items-center transition`}
+>
+  <!-- Content -->
+  <div class="bg-white p-8 max-w-sm w-full rounded-lg">
+    <header>
+      <h3 class="text-xl font-bold">Cancel Confirmation</h3>
+    </header>
+
+    <section class="mt-2">
+      <p>Are you sure you want to cancel your booked class?</p>
+
+      <footer class="flex space-x-2 mt-4 justify-end">
+        <button
+          class="text-blue-500 px-4 py-2 rounded-md"
+          on:click={() => {
+            showModal = false;
+          }}>Cancel</button
+        >
+        <button
+          class="bg-red-500 text-white px-4 py-2 rounded-md"
+          on:click={handleCancel}
+        >
+          Yes
+        </button>
+      </footer>
+    </section>
+  </div>
+</div>
 
 <header class="flex justify-between">
   <h2 class="text-2xl">Class History</h2>
