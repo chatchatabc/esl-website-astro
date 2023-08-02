17015fcc16ffb97f8e7e212a002f0d4865622743
BoJoNVi <bonjomontes@gmail.com>
2023-08-02 11:42:27 +0800

    improv: responsive design for the profile information

--- src/components/profiles/ProfileInformation.svelte
+++ src/components/profiles/ProfileInformation.svelte
@@ -8,24 +8,29 @@
   <h2 class="text-2xl">Profile</h2>
 </header>
 
-<section class="flex -mx-2">
-  <div class="p-2 w-1/4">
+<section class="flex flex-wrap -mx-2">
+  <div class="p-2 w-1/2 md:w-1/3 lg:w-1/5">
     <h3 class="text-xs font-bold">First name</h3>
     <p class="capitalize">{user.firstName}</p>
   </div>
 
-  <div class="p-2 w-1/4">
+  <div class="p-2 w-1/2 md:w-1/3 lg:w-1/5">
     <h3 class="text-xs font-bold">Last name</h3>
     <p class="capitalize">{user.lastName}</p>
   </div>
 
-  <div class="p-2 w-1/4">
+  <div class="p-2 w-1/2 md:w-1/3 lg:w-1/5">
     <h3 class="text-xs font-bold">Username</h3>
     <p>{user.username}</p>
   </div>
 
-  <div class="p-2 w-1/4">
+  <div class="p-2 w-1/2 md:w-1/3 lg:w-1/5">
     <h3 class="text-xs font-bold">Phone</h3>
     <p class="capitalize">{user.phone}</p>
+  </div>
+
+  <div class="p-2 w-1/2 md:w-1/3 lg:w-1/5">
+    <h3 class="text-xs font-bold">Credit</h3>
+    <p class="capitalize">{user.credit}元</p>
   </div>
 </section>
