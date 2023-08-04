c064bf15db6956b2c963fb57f5661a7c23ec8c9f
BoJoNVi <bonjomontes@gmail.com>
2023-08-04 10:01:00 +0800

    feat(client): dynamic pagination component

---
+++ src/components/widgets/Pagination.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let totalElements: number,
+    page: number,
+    size: number,
+    handleChange: (page: number) => void;
+
+  const totalPages = Math.ceil(totalElements / size) + 1;
+</script>
+
+<div class="flex space-x-2 items-center">
+  <p>{page + 1} / {totalPages}</p>
+
+  <div class="space-x-1 flex items-center">
+    <button
+      class={`h-8 w-8 rounded-full bg-blue-500 text-white ${
+        page === 0 && "opacity-50"
+      }`}
+      disabled={page === 0}
+      on:click={() => {
+        handleChange(page - 1);
+      }}
+    >
+      &lt;
+    </button>
+
+    <button
+      class={`h-8 w-8 rounded-full bg-blue-500 text-white ${
+        page + 1 === totalPages && "opacity-50"
+      }`}
+      disabled={page + 1 === totalPages}
+      on:click={() => {
+        handleChange(page + 1);
+      }}
+    >
+      &gt;
+    </button>
+  </div>
+</div>
