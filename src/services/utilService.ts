0862b67aa942f91517905c1fce153eb1a35c0971
BoJoNVi <bonjomontes@gmail.com>
2023-08-14 10:37:26 +0800

    feat: utils to save and get values from cookie

--- src/services/utilService.ts
+++ src/services/utilService.ts
@@ -21,3 +21,27 @@ export function utilHandleTrpcError(error: any) {
     ],
   };
 }
+
+export function utilCookieSave(
+  name: string,
+  value: any,
+  maxAge: number = 3600
+) {
+  value = JSON.stringify(value);
+  document.cookie = `${name}=${value}; path=/; max-age=${maxAge}`;
+}
+
+export function utilCookieGet(name: string) {
+  const cookie = document.cookie;
+  const value = cookie
+    .split("; ")
+    .find((item) => item.includes(`${name}=`))
+    ?.trim()
+    .slice(name.length + 1);
+
+  if (!value) {
+    return undefined;
+  }
+
+  return JSON.parse(value);
+}
