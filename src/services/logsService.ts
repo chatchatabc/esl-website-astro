94a0b65724643180db9c948cba1d8174e99e7f31
BoJoNVi <bonjomontes@gmail.com>
2023-09-15 17:35:39 +0800

    fix: bug in getting credit logs

--- src/services/logsService.ts
+++ src/services/logsService.ts
@@ -8,18 +8,11 @@ export async function logsGetAllCredit(params: {
     const response = await trpcClient.logs.getCreditAll.query(params);
 
     const contentPromise = response.content.map(async (logsCredit) => {
-      const sender = await trpcClient.user.get.query({
-        userId: logsCredit.senderId,
+      const user = await trpcClient.user.get.query({
+        userId: logsCredit.userId,
       });
-      if (sender) {
-        logsCredit.sender = sender;
-      }
-
-      const receiver = await trpcClient.user.get.query({
-        userId: logsCredit.receiverId,
-      });
-      if (receiver) {
-        logsCredit.receiver = receiver;
+      if (user) {
+        logsCredit.user = user;
       }
 
       return logsCredit;
@@ -34,32 +27,32 @@ export async function logsGetAllCredit(params: {
   }
 }
 
-export async function logsRequestCredit(params: { amount: number }) {
-  try {
-    const response = await trpcClient.logs.requestCredit.mutate(params);
-    return response;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-}
+// export async function logsRequestCredit(params: { amount: number }) {
+//   try {
+//     const response = await trpcClient.logs.requestCredit.mutate(params);
+//     return response;
+//   } catch (e) {
+//     console.log(e);
+//     return null;
+//   }
+// }
 
-export async function logsApproveCredit(params: { logId: number }) {
-  try {
-    const response = await trpcClient.logs.approveCredit.mutate(params);
-    return response;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-}
+// export async function logsApproveCredit(params: { logId: number }) {
+//   try {
+//     const response = await trpcClient.logs.approveCredit.mutate(params);
+//     return response;
+//   } catch (e) {
+//     console.log(e);
+//     return null;
+//   }
+// }
 
-export async function logsRejectCredit(params: { logId: number }) {
-  try {
-    const response = await trpcClient.logs.rejectCredit.mutate(params);
-    return response;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-}
+// export async function logsRejectCredit(params: { logId: number }) {
+//   try {
+//     const response = await trpcClient.logs.rejectCredit.mutate(params);
+//     return response;
+//   } catch (e) {
+//     console.log(e);
+//     return null;
+//   }
+// }
